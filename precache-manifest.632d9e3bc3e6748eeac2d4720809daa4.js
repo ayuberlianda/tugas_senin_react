8df020cf45bc29c6ddab245583d61061d1a1f0bc
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-11-02 22:14:51 +0700

    Add files via upload

---
+++ precache-manifest.632d9e3bc3e6748eeac2d4720809daa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c932e8133c8c99397f41bdf8a4b6c79c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d1572f2927a7763c1da",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "73896d44433794d8566b",
+    "url": "/static/js/2.f562380b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f562380b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1572f2927a7763c1da",
+    "url": "/static/js/main.10b7e517.chunk.js"
+  },
+  {
+    "revision": "ea2431c578d3a9ebaa9f",
+    "url": "/static/js/runtime-main.2fc453ef.js"
+  }
+]);
